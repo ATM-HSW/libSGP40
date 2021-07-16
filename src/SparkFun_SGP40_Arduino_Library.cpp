1cb8008634cd9e790987f87bce474f7629c4df9a
Olaf Hagendorf <olaf.hagendorf@hs-wismar.de>
2021-07-16 12:53:13 +0200

    initial commit

--- src/SparkFun_SGP40_Arduino_Library.cpp
+++ src/SparkFun_SGP40_Arduino_Library.cpp
@@ -40,20 +40,17 @@
   THE SOFTWARE.
 */
 
-
 #include "SparkFun_SGP40_Arduino_Library.h"
 
 
 //Constructor
-SGP40::SGP40()
-{
+SGP40::SGP40() {
 }
 
 //Start I2C communication using specified port
 //Returns true if successful or false if no sensor detected
-bool SGP40::begin(TwoWire &wirePort)
-{
-  _i2cPort = &wirePort; //Grab which port the user wants us to use
+bool SGP40::begin(I2C &i2c) {
+  _i2c = &i2c; //Grab which port the user wants us to use
 
   VocAlgorithm_init(&vocAlgorithmParameters); //Initialize the VOC parameters
 
@@ -63,68 +60,46 @@ bool SGP40::begin(TwoWire &wirePort)
 
 //Calling this function with nothing sets the debug port to Serial
 //You can also call it with other streams like Serial1, SerialUSB, etc.
-void SGP40::enableDebugging(Stream &debugPort)
-{
-	_debugPort = &debugPort;
-	_printDebug = true;
+void SGP40::enableDebugging(bool on) {
+	_printDebug = on;
 }
 
 //Sensor runs on chip self test
 //Returns SUCCESS (0) if successful or other error code if unsuccessful
-SGP40ERR SGP40::measureTest(void)
-{
-  _i2cPort->beginTransmission(_SGP40Address);
-  _i2cPort->write(sgp40_measure_test, 2); //Perform a measurement test
-  uint8_t i2cResult = _i2cPort->endTransmission();
+SGP40ERR SGP40::measureTest(void) {
 
-  if (i2cResult != 0)
-  {
+  i2cResult = _i2c->write(_SGP40Address, (const char*)sgp40_measure_test, 2);
+
+  if (i2cResult != 0) {
     if (_printDebug == true)
-    {
-      _debugPort->print(F("measureTest: endTransmission returned: "));
-      _debugPort->println(i2cResult);
-    }
+      printf("measureTest: endTransmission returned: %d\n",i2cResult);
     return SGP40_ERR_I2C_ERROR;
   }
 
   //Hang out while measurement is taken. datasheet says 250ms
-  delay(250);
+  thread_sleep_for(250);
 
   //Comes back in 3 bytes, data(MSB) / data(LSB) / Checksum
-  uint8_t toRead = _i2cPort->requestFrom(_SGP40Address, (uint8_t)3);
-  if (toRead != 3)
-  {
+  i2cResult = _i2c->read(_SGP40Address, (char*)_buf, 3);
+  if (i2cResult != 0) {
     if (_printDebug == true)
-    {
-      _debugPort->print(F("measureTest: requestFrom returned: "));
-      _debugPort->println(toRead);
-    }
+      printf("measureTest: requestFrom returned: %d\n", i2cResult);
     return SGP40_ERR_I2C_ERROR; //Error out
   }
 
-  uint16_t results = ((uint16_t)_i2cPort->read()) << 8; //store MSB in results
-  results |= _i2cPort->read(); //store LSB in results
+  uint16_t results = ((uint16_t)_buf[0]) << 8; //store MSB in results
+  results |= _buf[1]; //store LSB in results
 
-  uint8_t checkSum = _i2cPort->read(); //verify checksum
-  if (checkSum != _CRC8(results))
-  {
+  uint8_t checkSum = _buf[2]; //verify checksum
+  if (checkSum != _CRC8(results)) {
     if (_printDebug == true)
-    {
-      _debugPort->print(F("measureTest: checksum failed! Expected: 0x"));
-      _debugPort->print(_CRC8(results), HEX);
-      _debugPort->print(F(" Received: 0x"));
-      _debugPort->println(checkSum, HEX);
-    }
+      printf("measureTest: checksum failed! Expected: 0x%x Received: 0x%x\n", _CRC8(results), checkSum);
     return SGP40_ERR_BAD_CRC; //checksum failed
   }
 
-  if (results != sgp40_measure_test_pass)
-  {
+  if (results != sgp40_measure_test_pass) {
     if (_printDebug == true)
-    {
-      _debugPort->print(F("measureTest: unexpected test results! Received: 0x"));
-      _debugPort->println(results, HEX);
-    }
+      printf("measureTest: unexpected test results! Received: 0x%x\n", results);
     return SGP40_MEASURE_TEST_FAIL; //self test results incorrect
   }
 
@@ -133,19 +108,13 @@ SGP40ERR SGP40::measureTest(void)
 
 //Perform a soft reset
 //Returns SUCCESS (0) if successful
-SGP40ERR SGP40::softReset(void)
-{
-  _i2cPort->beginTransmission(_SGP40Address);
-  _i2cPort->write(sgp40_soft_reset, 2); //Perform a soft reset
-  uint8_t i2cResult = _i2cPort->endTransmission();
+SGP40ERR SGP40::softReset(void) {
+  
+  i2cResult = _i2c->write(_SGP40Address, (const char*)sgp40_soft_reset, 2); //Perform a soft reset
 
-  if (i2cResult != 0)
-  {
-    if (_printDebug == true)
-    {
-      _debugPort->print(F("softReset: endTransmission returned: "));
-      _debugPort->println(i2cResult);
-    }
+  if (i2cResult != 0) {
+    if (_printDebug == true) 
+      printf("softReset: endTransmission returned: %d\n", i2cResult);
     return SGP40_ERR_I2C_ERROR;
   }
 
@@ -154,21 +123,15 @@ SGP40ERR SGP40::softReset(void)
 
 //Turn the heater off
 //Returns SUCCESS (0) if successful
-SGP40ERR SGP40::heaterOff(void)
-{
-  _i2cPort->beginTransmission(_SGP40Address);
-  _i2cPort->write(sgp40_heater_off, 2); //Turn the heater off
-  uint8_t i2cResult = _i2cPort->endTransmission();
+SGP40ERR SGP40::heaterOff(void) {
 
-  delay(1); // From datasheet
+  i2cResult = _i2c->write(_SGP40Address, (const char*)sgp40_heater_off, 2); //Perform a soft reset
 
-  if (i2cResult != 0)
-  {
+  thread_sleep_for(1); // From datasheet
+
+  if (i2cResult != 0) {
     if (_printDebug == true)
-    {
-      _debugPort->print(F("heaterOff: endTransmission returned: "));
-      _debugPort->println(i2cResult);
-    }
+      printf("heaterOff: endTransmission returned: %d\n", i2cResult);
     return SGP40_ERR_I2C_ERROR;
   }
 
@@ -187,97 +150,70 @@ SGP40ERR SGP40::heaterOff(void)
 //Default %RH = 50 (RH_ticks = 0x8000)
 //Default DegC = 25 (T_ticks = 0x6666)
 //See the SGP40 datasheet for more details
-SGP40ERR SGP40::measureRaw(uint16_t *SRAW_ticks, float RH, float T)
-{
+SGP40ERR SGP40::measureRaw(uint16_t *SRAW_ticks, float RH, float T) {
   uint16_t RH_ticks, T_ticks;
 
   // Check RH and T are within bounds (probably redundant, but may prevent unexpected wrap-around errors)
-  if (RH < 0)
-  {
+  if (RH < 0) {
     if (_printDebug == true)
-      _debugPort->println(F("measureRaw: RH too low! Correcting..."));
+      printf("measureRaw: RH too low! Correcting...\n");
     RH = 0;
   }
-  if (RH > 100)
-  {
+  if (RH > 100) {
     if (_printDebug == true)
-      _debugPort->println(F("measureRaw: RH too high! Correcting..."));
+      printf("measureRaw: RH too high! Correcting...\n");
     RH = 100;
   }
-  if (T < -45)
-  {
+  if (T < -45) {
     if (_printDebug == true)
-      _debugPort->println(F("measureRaw: T too low! Correcting..."));
+      printf("measureRaw: T too low! Correcting...\n");
     T = -45;
   }
-  if (T > 130)
-  {
+  if (T > 130) {
     if (_printDebug == true)
-      _debugPort->println(F("measureRaw: T too high! Correcting..."));
+      printf("measureRaw: T too high! Correcting...\n");
     T = 130;
   }
 
   RH_ticks = (uint16_t)(RH * 65535 / 100); // Convert RH from %RH to ticks
   T_ticks = (uint16_t)((T + 45) * 65535 / 175); // Convert T from DegC to ticks
   if (_printDebug == true)
-  {
-    _debugPort->print(F("measureRaw: RH_ticks: 0x"));
-    _debugPort->print(RH_ticks, HEX);
-    _debugPort->print(F(" T_ticks: 0x"));
-    _debugPort->println(T_ticks, HEX);
-  }
+    printf("measureRaw: RH_ticks: 0x%x T_ticks: 0x%x\n", RH_ticks, T_ticks);
 
-  _i2cPort->beginTransmission(_SGP40Address);
-  _i2cPort->write(sgp40_measure_raw, 2); //Perform a raw measurement
+  _buf[0] = sgp40_measure_raw[0];       //Perform a raw measurement
+  _buf[1] = sgp40_measure_raw[1];
+  _buf[2] = (uint8_t)(RH_ticks >> 8);   //Write the RH_ticks MSB
+  _buf[3] = (uint8_t)(RH_ticks & 0xFF); //Write the RH_ticks LSB
+  _buf[4] = _CRC8(RH_ticks);            //Write the CRC
+  _buf[5] = (uint8_t)(T_ticks >> 8);    //Write the T_ticks MSB
+  _buf[6] = (uint8_t)(T_ticks & 0xFF);  //Write the T_ticks LSB
+  _buf[7] = _CRC8(T_ticks);             //Write the CRC
+  i2cResult = _i2c->write(_SGP40Address, (const char*)_buf, 8);
 
-  _i2cPort->write((uint8_t)(RH_ticks >> 8)); //Write the RH_ticks MSB
-  _i2cPort->write((uint8_t)(RH_ticks & 0xFF)); //Write the RH_ticks LSB
-  _i2cPort->write(_CRC8(RH_ticks)); //Write the CRC
-
-  _i2cPort->write((uint8_t)(T_ticks >> 8)); //Write the T_ticks MSB
-  _i2cPort->write((uint8_t)(T_ticks & 0xFF)); //Write the T_ticks LSB
-  _i2cPort->write(_CRC8(T_ticks)); //Write the CRC
-
-  uint8_t i2cResult = _i2cPort->endTransmission();
-
-  if (i2cResult != 0)
-  {
+  if (i2cResult != 0)   {
     if (_printDebug == true)
-    {
-      _debugPort->print(F("measureRaw: endTransmission returned: "));
-      _debugPort->println(i2cResult);
-    }
+      printf("measureRaw: endTransmission returned: %d\n", i2cResult);
     return SGP40_ERR_I2C_ERROR;
   }
 
   //Hang out while measurement is taken. datasheet says 30ms
-  delay(30);
+  thread_sleep_for(30);
 
   //Comes back in 3 bytes, data(MSB) / data(LSB) / Checksum
-  uint8_t toRead = _i2cPort->requestFrom(_SGP40Address, (uint8_t)3);
-  if (toRead != 3)
-  {
+  i2cResult = _i2c->read(_SGP40Address, (char*)_buf, 3);
+  if (i2cResult != 0) {
     if (_printDebug == true)
-    {
-      _debugPort->print(F("measureRaw: requestFrom returned: "));
-      _debugPort->println(toRead);
-    }
+      printf("measureRaw: requestFrom returned: %d\n", i2cResult);
     return SGP40_ERR_I2C_ERROR; //Error out
   }
 
-  uint16_t results = ((uint16_t)_i2cPort->read()) << 8; //store MSB in results
-  results |= _i2cPort->read(); //store LSB in results
+  uint16_t results = ((uint16_t)_buf[0]) << 8; //store MSB in results
+  results |= _buf[1]; //store LSB in results
 
-  uint8_t checkSum = _i2cPort->read(); //verify checksum
-  if (checkSum != _CRC8(results))
-  {
+  uint8_t checkSum = _buf[2]; //verify checksum
+  if (checkSum != _CRC8(results)) {
     if (_printDebug == true)
-    {
-      _debugPort->print(F("measureRaw: checksum failed! Expected: 0x"));
-      _debugPort->print(_CRC8(results), HEX);
-      _debugPort->print(F(" Received: 0x"));
-      _debugPort->println(checkSum, HEX);
-    }
+      printf("measureRaw: checksum failed! Expected: 0x%x Received: 0x%x\n", _CRC8(results), checkSum);
     return SGP40_ERR_BAD_CRC; //checksum failed
   }
 
@@ -289,20 +225,15 @@ SGP40ERR SGP40::measureRaw(uint16_t *SRAW_ticks, float RH, float T)
 //Get VOC Index
 //Returns -100 if an error occurs
 //The user can provide Relative Humidity and Temperature parameters if desired
-int32_t SGP40::getVOCindex(float RH, float T)
-{
+int32_t SGP40::getVOCindex(float RH, float T) {
   int32_t vocIndex;
   uint16_t SRAW_ticks;
 
   SGP40ERR result = measureRaw(&SRAW_ticks, RH, T);
 
-  if (result != SGP40_SUCCESS)
-  {
+  if (result != SGP40_SUCCESS) {
     if (_printDebug == true)
-    {
-      _debugPort->print(F("getVOCindex: measureRaw returned error: "));
-      _debugPort->println(result);
-    }
+      printf("getVOCindex: measureRaw returned error: %d\n", result);
     return -100; //fail...
   }
 
@@ -317,14 +248,12 @@ int32_t SGP40::getVOCindex(float RH, float T)
 //From: http://www.sunshine2k.de/articles/coding/crc/understanding_crc.html
 //Tested with: http://www.sunshine2k.de/coding/javascript/crc/crc_js.html
 //x^8+x^5+x^4+1 = 0x31
-uint8_t SGP40::_CRC8(uint16_t data)
-{
+uint8_t SGP40::_CRC8(uint16_t data) {
   uint8_t crc = 0xFF; //Init with 0xFF
 
   crc ^= (data >> 8); // XOR-in the first input byte
 
-  for (uint8_t i = 0 ; i < 8 ; i++)
-  {
+  for (uint8_t i = 0 ; i < 8 ; i++) {
     if ((crc & 0x80) != 0)
       crc = (uint8_t)((crc << 1) ^ 0x31);
     else
@@ -333,8 +262,7 @@ uint8_t SGP40::_CRC8(uint16_t data)
 
   crc ^= (uint8_t)data; // XOR-in the last input byte
 
-  for (uint8_t i = 0 ; i < 8 ; i++)
-  {
+  for (uint8_t i = 0 ; i < 8 ; i++) {
     if ((crc & 0x80) != 0)
       crc = (uint8_t)((crc << 1) ^ 0x31);
     else
@@ -344,14 +272,14 @@ uint8_t SGP40::_CRC8(uint16_t data)
   return crc; //No output reflection
 }
 #else
+
 //Generates CRC8 for SGP40 from lookup table
-uint8_t SGP40::_CRC8(uint16_t data)
-{
-  uint8_t CRC = 0xFF; //inital value
-  CRC ^= (uint8_t)(data >> 8); //start with MSB
-  CRC = _CRC8LookupTable[CRC >> 4][CRC & 0xF]; //look up table [MSnibble][LSnibble]
-  CRC ^= (uint8_t)data; //use LSB
-  CRC = _CRC8LookupTable[CRC >> 4][CRC & 0xF]; //look up table [MSnibble][LSnibble]
-  return CRC;
+uint8_t SGP40::_CRC8(uint16_t data) {
+  uint8_t crc = (uint8_t)0xFF; //inital value
+  crc ^= (uint8_t)(data >> 8); //start with MSB
+  crc = _CRC8LookupTable[crc >> 4][crc & 0xF]; //look up table [MSnibble][LSnibble]
+  crc ^= (uint8_t)data; //use LSB
+  crc = _CRC8LookupTable[crc >> 4][crc & 0xF]; //look up table [MSnibble][LSnibble]
+  return crc;
 }
 #endif
